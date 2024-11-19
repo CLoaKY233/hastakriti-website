33be78b2f5a95438c644d14717a558833a4cc645
CLoaKY233 <laysheth1@gmail.com>
2024-11-19 13:03:32 +0530

    height bug

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -57,7 +57,7 @@ export default async function ProjectsPage() {
 
         {/* Featured and Top Projects */}
         {featured ? (
-          <div className="grid grid-cols-1 gap-8 mx-auto h-80 lg:grid-cols-2 ">
+          <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 h-fit">
             {/* Featured Project */}
             <Card>
               <Link href={`/research/${featured.slug}`}>
